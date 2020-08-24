9b210c9c552f0683bc98fc027bb9830ce19d7697
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2020-08-24 14:42:21 -0400

    Try handling load more comments

--- content.js
+++ content.js
@@ -17,5 +17,43 @@ window.onload = function purgeKarma() {
       element.remove();
     })
   );
+
+  const moreComments = document.querySelectorAll(".morecomments");
+
+  // moreComments.forEach((moreComment) => {
+  //   const anchor = moreComment.querySelector("a");
+  //   const newAnchor = document.createElement("a");
+  //   newAnchor.innerText = moreLink.textContent;
+  //   newAnchor.style.fontWeight = "bold";
+  //   newAnchor.style.fontSize = "smaller";
+  //   newAnchor.style.cursor = "pointer";
+  //   newAnchor.setAttribute("href", "javascript:void(0)");
+  //   anchor.parentNode.replaceChild(newAnchor, anchor);
+  // });
+
+  moreComments.forEach((moreComment) => {
+    const anchor = moreComment.querySelector("a");
+    anchor.addEventListener("click", () => {
+      const karmaIdentifiers = [
+        ".score",
+        ".karma",
+        ".arrow",
+        ".userkarma",
+        ".awarding-link",
+        ".awarding-show-more-link",
+        ".author-tooltip__karma",
+        ".author",
+      ];
+
+      setTimeout(() => {
+        karmaIdentifiers.forEach((identifier) =>
+          document.querySelectorAll(identifier).forEach((element) => {
+            element.remove();
+          })
+        );
+      }, 2000);
+    });
+  });
+
   document.documentElement.style.visibility = "";
 };
