529720432abbc84a0438dc9c57b695832d525b37
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2020-08-30 16:12:25 -0400

    Listen for changes to settings

--- options/options.js
+++ options/options.js
@@ -4,6 +4,30 @@ const defaultSettings = {
   hideUsernames: false,
   hideOwnKarma: true,
 };
+const settings = chrome.storage.sync;
+const checkboxes = document.querySelectorAll(
+  '.karmaless-setting input[type="checkbox"'
+);
+
+function getSettingName(checkbox) {
+  return checkbox.getAttribute("name");
+}
+
+function onSettingToggled(changeEvent) {
+  const settingName = getSettingName(changeEvent.target);
+  settings.set({ [settingName]: changeEvent.target.checked });
+}
+
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", onSettingToggled);
+
+  const settingName = getSettingName(checkbox);
+  settings.get({ [settingName]: defaultSettings[settingName] }, (result) => {
+    if (result[settingName]) {
+      checkbox.setAttribute("checked", true);
+    }
+  });
+});
 
 // function save_options() {
 //   var color = document.getElementById("color").value;
