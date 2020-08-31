b8a038caefef63e69370578f243648b506a6b580
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2020-08-31 08:22:04 -0400

    Copy HTML, CSS, and manifest to dist folder and update manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -17,16 +17,22 @@ module.exports = {
     path: path.resolve(__dirname, 'dist/'),
     filename: '[name].bundle.js',
   },
+  // https://stackoverflow.com/a/49100966/5323344
+  devtool: 'cheap-module-source-map',
   plugins: [
     new CopyPlugin({
       patterns: [
         {
-          from: path.join(__dirname, 'src/**/*.html'),
-          to: path.join(__dirname, 'dist/static'),
+          from: '**/*.html',
+          context: path.resolve(__dirname, 'src', 'options'),
         },
         {
-          from: path.join(__dirname, 'src/**/*.css'),
-          to: path.join(__dirname, 'dist/static'),
+          from: '**/*.css',
+          context: path.resolve(__dirname, 'src', 'options'),
+        },
+        {
+          from: 'manifest.json',
+          context: path.resolve(__dirname),
         },
       ],
     }),
