8015576f856bc374d8be41a0467d08334a107eb0
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2020-09-05 09:09:28 -0400

    Remove updateCheckbox dependency from writeSetting and return a promise instead

--- src/store/writeSetting.js
+++ src/store/writeSetting.js
@@ -1,5 +1,4 @@
 import { store } from 'store/store';
-import updateCheckbox from 'utils/updateCheckbox';
 
 /**
  *
@@ -8,5 +7,5 @@ import updateCheckbox from 'utils/updateCheckbox';
  */
 export default function writeSetting(settingName, value) {
   store.set({ [settingName]: value });
-  updateCheckbox(settingName, value);
+  return new Promise((resolve) => resolve());
 }
